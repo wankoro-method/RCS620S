5724a86870bd7b87a04deca04b4810a009e835af
mayu___cs <hato182@gmail.com>
2021-07-17 23:21:50 +0900

    Update RCS620S_R_W.ino

--- RCS620S_R_W/RCS620S_R_W.ino
+++ RCS620S_R_W/RCS620S_R_W.ino
@@ -5,27 +5,21 @@
 
 #include <string.h>
 
-//LED
-const int LED = 2;
+const int LED = 2;                              //LED
+const unsigned int IO_wait_time_ms = 250;       //Wait time
 
-//Wait time
-const unsigned int IO_wait_time_ms = 250;
+RCS620S rc_s620s;                               //RC-S620S Device class
+RCS620SCommand rc_s620s_cmd;                    //RC-S620S Command class
 
-//RC-S620S Device class
-RCS620S rc_s620s;
-RCS620SCommand rc_s620s_cmd;
+uint8_t response[RCS620S_MAX_RW_RESPONSE_LEN];  //レスポンスデータキャッチ用
+uint8_t responseLen;
+
+bool executeFlag = false;                       //前フレームIDmデータ比較用
+uint8_t beforeFrameIDm[8] = { 0 };
 
 void ShowIDm();
 void ShowPMm();
 void ShowData();
-
-//レスポンスデータキャッチ用
-uint8_t response[RCS620S_MAX_RW_RESPONSE_LEN];
-uint8_t responseLen;
-
-//前フレームIDmデータ比較用
-bool executeFlag = false;
-uint8_t beforeFrameIDm[8] = { 0 };
 
 //Initialize
 void setup() {
@@ -35,59 +29,77 @@ void setup() {
   rc_s620s.initDevice();
 }
 
+String data;
+
 void loop() {
   for(int i = 0; i < 8; i++){
     beforeFrameIDm[i] = rc_s620s.idm[i];
   }
 
-  if (!rc_s620s.polling(SystemCode::code_wild_card)) {
-    rc_s620s.rfOff();
-    analogWrite(LED, LOW);
-    delay(IO_wait_time_ms);
-    return; 
+  if(!Serial.available()) { digitalWrite(LED, LOW); return; }
+
+  digitalWrite(LED, HIGH);
+
+  data = "";
+  while(Serial.available()){
+    char cache = Serial.read();
+    data += cache;
   }
 
-  if(memcmp(beforeFrameIDm, rc_s620s.idm, 8) == 0){
-    digitalWrite(LED, HIGH);
-    rc_s620s.rfOff();
-    delay(IO_wait_time_ms);
-    return; 
-  }
-
-  ShowData();
-  uint8_t data[] = { 0x11, 0x45, 0x14, 0x81, 0x01, 0x91, 0x9F};
-  rc_s620s_cmd.CreateDataWriteCommand(rc_s620s.idm, 1, ServiceCode::RWAccess, 1, RWBlock::S_PAD13, data, sizeof(data));
-
-  if(rc_s620s.cardCommand(rc_s620s_cmd.cmdList, rc_s620s_cmd.cmdListLen, response, &responseLen) == 1){
-    for (int i = 0; i < responseLen; i++) {
-      Serial.print(response[i], HEX);
-
-      if(i != responseLen - 1) { Serial.print(":"); }
+  if(data == "Pooling"){
+    if (rc_s620s.polling(SystemCode::code_wild_card)) {
+      ShowData();
+      rc_s620s.rfOff();
     }
-    Serial.println("");
-  }
-  else{
-    Serial.println("DataErr0r");
-  }
-
-
-  rc_s620s_cmd.CreateDataReadCommand(rc_s620s.idm, 0x01, ServiceCode::ROAccess, 0x01, RWBlock::S_PAD13);
-  
-  if(rc_s620s.cardCommand(rc_s620s_cmd.cmdList, rc_s620s_cmd.cmdListLen, response, &responseLen) == 1){
-    for (int i = 0; i < responseLen; i++) {
-      Serial.print(response[i], HEX);
-
-      if(i != responseLen - 1) { Serial.print(":"); }
+    else{
+      Serial.println("NFC card is nothing!!!");
+      rc_s620s.rfOff();
     }
-    Serial.println("");
   }
-  else{
-    Serial.println("DataErr0r");
+
+  if(data == "Read"){
+    if (!rc_s620s.polling(SystemCode::code_wild_card)) {
+      Serial.println("NFC card is nothing!!!");
+    }
+
+    rc_s620s_cmd.CreateDataReadCommand(rc_s620s.idm, 0x01, ServiceCode::ROAccess, 0x01, RWBlock::S_PAD13);
+
+    if(rc_s620s.cardCommand(rc_s620s_cmd.cmdList, rc_s620s_cmd.cmdListLen, response, &responseLen) == 1){
+      for (int i = 0; i < responseLen; i++) {
+        Serial.print(response[i], HEX);
+
+        if(i != responseLen - 1) { Serial.print(":"); }
+      }
+      Serial.println("");
+    }
+    else{
+      Serial.println("Read error");
+    }
+  }
+
+  if(data == "Write"){
+    if (!rc_s620s.polling(SystemCode::code_wild_card)) {
+      Serial.println("NFC card is nothing!!!");
+    }
+    
+    uint8_t data[] = { 0x11, 0x45, 0x14, 0x81, 0x01, 0x91, 0x9F};
+    rc_s620s_cmd.CreateDataWriteCommand(rc_s620s.idm, 1, ServiceCode::RWAccess, 1, RWBlock::S_PAD13, data, sizeof(data));
+
+    if(rc_s620s.cardCommand(rc_s620s_cmd.cmdList, rc_s620s_cmd.cmdListLen, response, &responseLen) == 1){
+      for (int i = 0; i < responseLen; i++) {
+        Serial.print(response[i], HEX);
+
+        if(i != responseLen - 1) { Serial.print(":"); }
+      }
+      Serial.println("");
+    }
+    else{
+      Serial.println("Write error");
+    }
   }
 
   //切断処理
   rc_s620s.rfOff();
-  delay(IO_wait_time_ms);
 }
 
 void ShowIDm()
